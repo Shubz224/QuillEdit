03c82686b577428d762c1396a24693e2ba7aef67
Shubham Nikam <102355393+Shubz224@users.noreply.github.com>
2024-05-22 16:12:11 +0530

    setting up qill and solved   problem of rendring again and again while adding new code

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useCallback} from 'react'
+import Quill from 'quill'
+import "quill/dist/quill.snow.css"
+
 
 export default function TextEditor() {
-  return (
-    <div>
-      Text Editor
-    </div>
-  )
+
+const wrapperRef  = useCallback(wrapper =>{
+  if(wrapper == null) return ;
+  wrapper.innerHtml = ""
+  const editor = document.createElement('div');
+  wrapper.append(editor)
+  new Quill (editor,{theme :'snow'})
+},[])
+return <div className = "container"  ref = {wrapperRef}></div>
+
 }
