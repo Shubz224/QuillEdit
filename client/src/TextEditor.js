48814b55ba96190455aea3578cdac33c61183173
Shubham Nikam <102355393+Shubz224@users.noreply.github.com>
2024-05-31 19:24:42 +0530

    Added seperate rooms for each user according to params

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,10 +1,10 @@
-import React, { useCallback , useEffect,useState} from 'react'
-import Quill from 'quill'
-import "quill/dist/quill.snow.css"
-import {io} from 'socket.io-client'
+import React, { useCallback, useEffect, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
 
-
-//setting up toolbar 
+//setting up toolbar
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
   [{ font: [] }],
@@ -15,86 +15,84 @@ const TOOLBAR_OPTIONS = [
   [{ align: [] }],
   ["image", "blockquote", "code-block"],
   ["clean"],
-]
+];
 
 export default function TextEditor() {
+  const { id: documentId } = useParams();
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
 
-const [socket,setSocket] = useState();
-const [quill ,setQuill] = useState();
+  console.log(documentId);
+  //creating socket and disconnecting on its own after text editor unmount
+  useEffect(() => {
+    const s = io("http://localhost:3001");
+    setSocket(s);
+    return () => {
+      s.disconnect();
+    };
+  }, []);
 
+  //for creating user with seperate user rooms
+  useEffect(() => {
+    if (socket == null || quill == null) return;
 
-//creating socket and disconnecting on its own after text editor unmount
-  useEffect(()=>{
-    const s= io("http://localhost:3001")
-    setSocket(s) ;
-    return ()=>{
-      s.disconnect()
-    }
-  },[]);
+    socket.once("load-document", (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
 
-  useEffect(()=>{
+    socket.emit("get-document", documentId);
+  }, [socket, quill, documentId]);
 
+  useEffect(() => {
     // making sure we are having socket and qill before updating any delta
-    if(socket == null || quill == null )return 
-  
-    const handler = (delta)=>{
-      quill.updateContents(delta)
-    }
-  
+    if (socket == null || quill == null) return;
+
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+
     //eventlistner for whenever qill  has  text that changes  we will send  it to or emit it to client side
-    socket.on('receive-changes',handler)
-  
-    return ()=>{
-      socket.off('receive-changes',handler)
-    }
-  },[socket,quill])
+    socket.on("receive-changes", handler);
 
+    return () => {
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
 
+  // function to track text change
 
+  useEffect(() => {
+    // making sure we are having socket and qill before updating any delta
+    if (socket == null || quill == null) return;
 
+    const handler = (delta, oldDelta, source) => {
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
 
+    //eventlistner for whenever qill  has  text that changes  we will send  it to or emit it to client side
+    quill.on("text-change", handler);
 
+    return () => {
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
 
-
-
-
-
-
-
-// function to track text change
-
-
-useEffect(()=>{
-
-  // making sure we are having socket and qill before updating any delta
-  if(socket == null || quill == null )return 
-
-  const handler = (delta,oldDelta,source)=>{
-    if(source !== 'user')return 
-    socket.emit('send-changes',delta);
-  }
-
-  //eventlistner for whenever qill  has  text that changes  we will send  it to or emit it to client side
-  quill.on('text-change',handler)
-
-  return ()=>{
-    quill.off('text-change',handler)
-  }
-},[socket,quill])
-
-
-  //setting up quill 
-const wrapperRef  = useCallback(wrapper =>{
-
-  if(wrapper == null) return ;
-  wrapper.innerHtml = ""
-  const editor = document.createElement('div');
-  wrapper.append(editor)
-  const q = new Quill (editor,{theme :'snow', modules: {toolbar:
-    TOOLBAR_OPTIONS}})
+  //setting up quill
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
+    wrapper.innerHtml = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      theme: "snow",
+      modules: { toolbar: TOOLBAR_OPTIONS },
+    });
+    q.disable();
+    q.setText("Loading...");
 
     setQuill(q);
-},[])
-return <div className = "container"  ref = {wrapperRef}></div>
-
+  }, []);
+  return <div className="container" ref={wrapperRef}></div>;
 }
--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
-const io = require("socket.io")(
-    3001,{
-        cors:{
-            origin :"http://localhost:3000",
-            method :["GET","POST"],
-        },
-    }
-)
+const io = require("socket.io")(3001, {
+  cors: {
+    origin: "http://localhost:3000",
+    method: ["GET", "POST"],
+  },
+});
 
-io.on("connection",socket =>{
-    socket.on('send-changes', delta =>{
-        socket.broadcast.emit('receive-changes',delta)
+io.on("connection", socket => {
+  socket.on("get=document", documentId => {
+    const data = "";
+    socket.join(documentId);
+          
+    socket.emit('load-document',data)
+
+
+    socket.on("send-changes", delta => {
+      socket.broadcast.to(documentId).emit("receive-changes", delta);
     })
-   
+  })
 })
