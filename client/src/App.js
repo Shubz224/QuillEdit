869b9810873d938142c4658ada0b99df516b1b47
Shubham Nikam <102355393+Shubz224@users.noreply.github.com>
2024-05-31 18:46:14 +0530

    Added random url (uuidv4) id for distinct users

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,28 @@
 import TextEditor from "./TextEditor";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import { v4 as uuidV4 } from 'uuid';
 
 function App() {
-  return <TextEditor/>
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={<Navigate to={`/documents/${uuidV4()}`} replace />}
+        />
+        <Route
+          path="/documents/:id"
+          element={<TextEditor />}
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
